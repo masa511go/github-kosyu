987493ae93814bf551b0044b070f11c967038392
masa511go <masa511go@gmail.com>
2025-12-19 20:42:31 +0900

    カレンダーを表示するプログラムを追加

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import datetime
+import calendar
 print("Hello,world!!")
 print("We create a develop branch")
 print("This branch is masa511go/feat/print-today")
 now_date = datetime.datetime.now().date()
+now_year = datetime.datetime.now().year
+now_month = datetime.datetime.now().month
+print(calendar.month(now_year,now_month))
 print(now_date)
